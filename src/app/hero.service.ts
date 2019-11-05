ca916a9db900ee3333ae922dfe49faa6c2af4f79
lantos14 <lantos891213@gmail.com>
2019-11-05 16:38:16 +0100

    FEAT: finish tour-of-heroes tutorial

---
+++ src/app/hero.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Hero } from './hero/hero';
+import { HEROES } from './hero/hero-list';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched Hero id: ${id}`);
+    return of (HEROES.find(hero => hero.id === id))
+  }
+  constructor(
+    private messageService: MessageService
+  ) { }
+}
