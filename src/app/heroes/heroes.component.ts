4f145c23227fdde55189daf77e10127bc5285fa0
lantos14 <lantos891213@gmail.com>
2019-11-06 09:24:18 +0100

    REFACT: hero list component routing to details page

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero/hero';
 import { HeroService } from '../services/hero/hero.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -11,16 +12,21 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
   constructor(
-    private heroService: HeroService
+    private heroService: HeroService,
+    private router: Router,
   ) { }
 
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-  
+
   ngOnInit() {
     this.getHeroes();
   }
 
+  onHeroClick(hero): void {
+    console.log(hero);
+    this.router.navigateByUrl(`/detail/${hero.id}`);
+  }
 }
