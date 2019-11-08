43b8b45760c5a7ed8f74c49382e206674a7505e0
lantos14 <lantos891213@gmail.com>
2019-11-08 13:22:46 +0100

    FEAT: add ngStyle solution for hero-name-colorizer

--- src/app/pipes/hero-name-colorizer.pipe.ts
+++ src/app/pipes/hero-name-colorizer.pipe.ts
@@ -1,23 +1,27 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Hero } from '../hero/hero';
 
+// this pipe adds a class to the template, but it can be used only
+// with ViewEncapsulation.None on the Component, which cause the 
+// class to flow out to the whole document
 @Pipe({
   name: 'heroNameColorizer'
 })
 export class HeroNameColorizerPipe implements PipeTransform {
-
   transform(hero: Hero): string {
     const { name, alignment } = hero;
     let classString = '';
 
     if (alignment === 'good') {
       classString = 'blue';
-    } else if(alignment === 'evil') {
+    } else if (alignment === 'evil') {
       classString = 'red';
-    } else {
+    } else if (alignment === 'neutral') {
       classString = 'gray';
     }
-    return `<span class="${classString}">${name}</span>`;
+
+    //  return `<span class="${classString}">${name}</span>`;
+    return `<span style="color: ${classString}">${name}</span>`;
   }
 
 }
