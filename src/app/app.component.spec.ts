304a74b171de5ba275f07edd108c074c13c76408
lantos14 <lantos891213@gmail.com>
2019-11-08 15:38:19 +0100

    FIX: app component test import issues

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,48 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroFilterComponent } from './heroes/hero-filter/hero-filter.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MessagesIntervalComponent } from './messages-interval/messages-interval.component';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+
+import { HeroNameColorizerPipe } from './pipes/hero-name-colorizer.pipe';
+import { SafeHtmlPipe } from './pipes/safe-html.pipe';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent }
+];
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         FormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes(routes),
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        DashboardComponent,
+        HeroDetailComponent,
+        HeroesComponent,
+        HeroFilterComponent,
+        MessagesComponent,
+        MessagesIntervalComponent,
+        HeroNameColorizerPipe,
+        SafeHtmlPipe
       ],
     }).compileComponents();
   }));
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroFilterComponent } from './heroes/hero-filter/hero-filter.component';
 import { MessagesIntervalComponent } from './messages-interval/messages-interval.component';
 import { HeroNameColorizerPipe } from './pipes/hero-name-colorizer.pipe';
 import { SafeHtmlPipe } from './pipes/safe-html.pipe';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent,
     DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
     HeroFilterComponent,
+    MessagesComponent,
     MessagesIntervalComponent,
     HeroNameColorizerPipe,
     SafeHtmlPipe
@@ -29,10 +35,13 @@ import { SafeHtmlPipe } from './pipes/safe-html.pipe';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
+    RouterModule,
+    RouterModule.forRoot(routes),
     HttpClientModule,
   ],
-  providers: [],
+  exports: [
+    MessagesComponent,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
